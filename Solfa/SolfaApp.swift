0bbdb82bb0393c13b1879b8d4aa4acfc190d4082
mrzogy <magbolmrzogy@gmail.com>
2023-06-13 10:40:07 +0300

    Initial Commit

---
+++ Solfa/SolfaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolfaApp.swift
+//  Solfa
+//
+//  Created by 3bood on 24/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct SolfaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
