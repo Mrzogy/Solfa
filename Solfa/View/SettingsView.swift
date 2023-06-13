d86462513ff2978ab710f0fce7cf0e300ca6fe53
mrzogy <magbolmrzogy@gmail.com>
2023-06-13 13:55:08 +0300

    Adding File To View Folder
    - ContentView
    - HomeView
    - OnboardingView
    - ProfileView
    - SettingsView
    - LaunchScreen

---
+++ Solfa/View/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  Solfa
+//
+//  Created by 3bood on 24/11/1444 AH.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
---
+++ Solfa/View/SignInView.swift
@@ -0,0 +1,20 @@
+//
+//  SignInView.swift
+//  Solfa
+//
+//  Created by 3bood on 24/11/1444 AH.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SignInView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignInView()
+    }
+}
